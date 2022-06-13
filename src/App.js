0f2cb7e3050ded772d1d0b17ce70683a600fa4af
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-06-13 16:53:50 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Login";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
-import Products from "./components/Products/Products";
-import { useSelector } from "react-redux";
 
 
 function App() {
-  const {isAuthenticated}=useSelector(state=>state.auth.data)
   return <div className="App">
     {/* code here */}
    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       
-      {isAuthenticated?
-      <Route path="/products" element={<Products/>}/>: null}
       <Route path="/login" element={<Login/>}/>
     </Routes>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Products from "../components/Products/Products";
+import Login from "./Login";
 
 const Home = () => {
+  const {isAuthenticated} = useSelector(state=>state.auth.data);
   return <div>{/* code here */}
-   <Products/>
+   {isAuthenticated?<Products/>:<Login/>}
   </div>;
 };
 
